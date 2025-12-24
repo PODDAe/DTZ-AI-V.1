558da5483a2fedae86c7f4489adf94893c9adb05
PODDAe <camalkaakash2@gmail.com>
2025-12-24 11:55:50 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,22 @@
+// Main JavaScript file
+const API_BASE = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3001' 
+    : '';
 
+// Application state
+let state = {
+    user: null,
+    token: null,
+    currentChat: null,
+    language: 'en',
+    model: 'deepseek'
+};
+
+// Initialize app
+async function initApp() {
+    await checkAuth();
+    setupEventListeners();
+    loadUI();
+}
+
+// ... rest of JavaScript code
